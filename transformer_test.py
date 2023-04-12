ce49a6d68540d31ed0bdce958ff433e7b165061a
CC-Cheung <cccheung.office@gmail.com>
2023-04-12 17:40:32 -0400

    doing wandb shit

--- transformer_test.py
+++ transformer_test.py
@@ -17,6 +17,7 @@ import pytorch_lightning as pl
 import numpy as np
 from torch.utils.data import TensorDataset, DataLoader
 from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.loggers import WandbLogger
 
 def make_quad_data(num_domains, num_points, coef):
   x=np.random.rand(num_domains, num_points, in_dim)
@@ -87,6 +88,7 @@ class TransformerBlock(nn.Module):
 class BaselineTrans(pl.LightningModule):
     def __init__(self, in_dim, embed_dim, out_dim, num_heads=1):
         super().__init__()
+        self.save_hyperparameters()
         self.transformer = TransformerBlock(in_dim, embed_dim, out_dim, num_heads=num_heads)
 
 
@@ -124,18 +126,22 @@ if __name__=="__main__":
     in_dim = 1
     hidden_dim_nature = 3
     hidden_dim_pred = 4
-    embed_dim = 5
+    embed_dim = 8
     num_heads = 1
     out_dim = 1
     depth = 2
-    train_coef = np.array([[1, 0, 1], [0, 1, 1], [1, 1, 0],[1, 0, 1], [0, 1, 1], [1, 1, 0],[1, 0, 1], [0, 1, 1], [1, 1, 0],[1, 0, 1], [0, 1, 1], [1, 1, 0]])
+    np.random.seed(0)
+    torch.use_deterministic_algorithms(True)
 
-    val_coef = np.array([[0.5, 0.5, .5]])
+    key="c20d41ecf28a9b0efa2c5acb361828d1319bc62e"
+
+    train_coef = np.array([[1, 0, 1], [0, 1, 1], [1, 1, 0],[1, 0, 1], [0, 1, 1], [1, 1, 0],[1, 0, 1], [0, 1, 1], [1, 1, 0],[1, 0, 1], [0, 1, 1], [1, 1, 0]])
+    train_coef=np.random.rand(100,3)
+    val_coef = np.array([[0.5, 0.1, .8]])
     num_points = 10
     num_domains = train_coef.shape[0]
-
     num_val_io_pairs=5
-
+    torch.manual_seed(0)
 
     train_dataset = TensorDataset(*make_quad_data(num_domains, num_points,train_coef))
     val_dataset = TensorDataset(*make_quad_data(1, num_points, val_coef))
@@ -143,13 +149,14 @@ if __name__=="__main__":
     val_loader = DataLoader(val_dataset,batch_size=10)
 
     tb_logger = pl_loggers.TensorBoardLogger(save_dir="lightning_logs/trash")
-
+    wandb_logger=WandbLogger(project="DA Thesis", log_model="all")
+    wandb_logger.watch()
     base_trans=BaselineTrans(in_dim+out_dim,embed_dim, 3, num_heads)
-    base_trans=BaselineTrans.load_from_checkpoint("lightning_logs/trash/lightning_logs/version_4/checkpoints/epoch=999-step=6000.ckpt",
-                                                in_dim=in_dim + out_dim, embed_dim=embed_dim, out_dim=3, num_heads=num_heads)
+    # base_trans=BaselineTrans.load_from_checkpoint("lightning_logs/trash/lightning_logs/version_9/checkpoints/epoch=999-step=50000.ckpt",
+    #                                             in_dim=in_dim + out_dim, embed_dim=embed_dim, out_dim=3, num_heads=num_heads)
 
     trainer1 = pl.Trainer(limit_train_batches=100,
-                     logger=tb_logger,
+                     logger=wandb_logger,
                       max_epochs=1000,
                       # log_every_n_steps = 5,
                     accelerator="gpu", devices=1,
